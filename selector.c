8394de44433f955dea5afcd56e93d76611a44bc8
DhyanCoder <45316023+DhyanCoder@users.noreply.github.com>
2020-01-31 18:33:11 +0530

    added some more files

---
+++ selector.c
@@ -0,0 +1,13 @@
+#include "filehander.c";
+
+/*
+Selects and prints the question and return answer as int
+97 for option a
+98 for option b
+99 for option c
+100 for option d
+*/
+
+int select() {
+    return 0; // for now
+}
