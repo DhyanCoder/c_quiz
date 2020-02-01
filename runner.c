6e538d758f77333b53c649714eaf6aec8066c183
DhyanCoder <45316023+DhyanCoder@users.noreply.github.com>
2020-02-01 23:20:14 +0530

    Removed some extra code

--- runner.c
+++ runner.c
@@ -3,10 +3,7 @@
 #include "selector.h"
 
 int main()
-{   
-    /*
-        print every rule and welcome things here.
-    */
+{
     int c, on = 0, q_num[20], len_q = 0;
     int num_of_q = 0, ask_this = 0, answer = 0; // FIXME: MODIFY THIS AFTER ADDING QUESTIONS
     int score = 0;
@@ -16,6 +13,7 @@ int main()
     c = getchar();
     switch(c) {
         case 's':
+            on = 1;
             break;
         case 'q':
             exit(0);
@@ -23,17 +21,13 @@ int main()
             printf("Invalid command");
     }
     } while (!on);
-    while ((c = getchar()) != 'q')
+    while (c != 'q')
     {
         ask_this = select(len_q, num_of_q, q_num);
         answer = handle(ask_this);
         c = getchar();
-        if (c == 'q') {
-            printf("Your total score is %d", score);
-            break;
-        }
         if (answer == c) {
-            printf("Well dome!\n");
+            printf("Well done!\n");
             score += 10 // FIXME:
         } else {
             printf("Wrong answer\nYour total score is %d", score);
